e34d14c270c263909482e7a9cfaaba918aa3bd29
MohdFaisal21 <faisalmohd1357@gmail.com>
2026-03-04 11:23:35 +0530

    install required pakages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const {PORT} = require("./config/serverConfig");
 const setupAndStartServer = async function(){
     const app = express();
+    const mysql = require("mysql2");
+    app.use(express.urlencoded({extended:true}));
+    app.use(express.json());
     app.listen(PORT, function(){
         console.log(`server listening on port ${PORT}`);
     });
